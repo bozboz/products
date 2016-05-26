dffbf1fb3565b5c91e2fd616259b8c97e9920ca2
Jim Hollington <jim@bozboz.co.uk>
2016-05-26 14:47:53 +0100

    Make default presenter route arg optional

--- src/Presenters/DefaultPresenter.php
+++ src/Presenters/DefaultPresenter.php
@@ -11,7 +11,7 @@ class DefaultPresenter implements Presentable
 {
     private $route;
 
-    public function __construct($route)
+    public function __construct($route = null)
     {
         $this->route = $route;
     }
@@ -26,6 +26,7 @@ class DefaultPresenter implements Presentable
         return [
             'ID' => sprintf('<strong>#%s</strong>', str_pad($instance->id, 3, '0', STR_PAD_LEFT)),
             'Name' => $instance->name,
+            'Status' => $instance->status ? '' : '<i class="fa fa-cross"></i>',
             'Added' => $instance->created_at ? sprintf('<abbr title="%s">%s</a>',
                 $instance->created_at->format('jS F Y'),
                 $instance->created_at->diffForHumans()
@@ -37,7 +38,7 @@ class DefaultPresenter implements Presentable
     {
         return [
             new TextField('name'),
-            new URLField('slug', ['route' => $this->route, 'data-auto-slug-from' => $instance->getSlugSourceField()]),
+            $this->route ? new URLField('slug', ['route' => $this->route, 'data-auto-slug-from' => $instance->getSlugSourceField()]) : null,
             new CheckboxField('status'),
         ];
     }
